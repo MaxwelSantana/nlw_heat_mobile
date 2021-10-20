2b82cbb21350cf11902884374854a9417214d8fc
Maxwel <maxwelsant96@icloud.com>
2021-10-20 19:51:08 -0300

    add react native svg transformer and expo svg

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+import LogoSvg from '../../assets/logo.svg';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <LogoSvg />
+      <Text>Sair</Text>
+    </View>
+  );
+}
