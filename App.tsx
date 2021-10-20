3326bd7b6214034e31fe3811f1fcbc908bd1af22
Maxwel <maxwelsant96@icloud.com>
2021-10-20 19:44:29 -0300

    add fonts and statusbar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 import { Home } from './src/screnns/Home';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <StatusBar style="light" />
+        <Home />
+      </>
+    );
+  }
 }
--- src/screnns/Home/styles.ts
+++ src/screnns/Home/styles.ts
@@ -1,9 +1,9 @@
 import { StyleSheet } from 'react-native';
+import { COLORS } from '../../theme';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: COLORS.BLACK_SECONDARY,
   },
 });
