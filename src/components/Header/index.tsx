10b372b389f83cc4d53eee389a6fb0c71bf04328
Maxwel <maxwelsant96@icloud.com>
2021-10-20 20:31:31 -0300

    finish header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,13 +8,12 @@ export function Header() {
   return (
     <View style={styles.container}>
       <LogoSvg />
-      <UserPhoto
-        imageUri="https://github.com/maxwelsantana.png"
-        sizes="SMALL"
-      />
-      <TouchableOpacity>
-        <Text style={styles.logoutText}>Sair</Text>
-      </TouchableOpacity>
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+        <UserPhoto imageUri="https://github.com/maxwelsantana.png" />
+      </View>
     </View>
   );
 }
