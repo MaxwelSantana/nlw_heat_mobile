58f02dd25273440721adc10262843778a576251f
Maxwel <maxwelsant96@icloud.com>
2021-10-20 20:53:44 -0300

    animation with moti

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { UserPhoto } from '../UserPhoto';
 import { styles } from './styles';
+import { MotiView } from 'moti';
 
 export type MessageProps = {
   id: string;
@@ -18,12 +19,17 @@ type Props = {
 
 export function Message({ data }: Props) {
   return (
-    <View style={styles.container}>
+    <MotiView
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: 'timing', duration: 700 }}
+      style={styles.container}
+    >
       <Text style={styles.message}>{data.text}</Text>
       <View style={styles.footer}>
         <UserPhoto sizes="SMALL" imageUri={data.user.avatar_url} />
         <Text style={styles.userName}>{data.user.name}</Text>
       </View>
-    </View>
+    </MotiView>
   );
 }
