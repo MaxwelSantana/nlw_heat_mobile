85db30e0dc3977914b1849ffc99e0980cdb23e2e
Maxwel <maxwelsant96@icloud.com>
2021-10-21 08:56:21 -0300

    adjust keyboard ios

--- src/screnns/Home/index.tsx
+++ src/screnns/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, KeyboardAvoidingView, Platform } from 'react-native';
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
 import { SendMessageForm } from '../../components/SendMessageForm';
@@ -9,11 +9,17 @@ import { styles } from './styles';
 
 export function Home() {
   const { isSigningIn, user } = useAuth();
+
   return (
-    <View style={styles.container}>
-      <Header />
-      <MessageList />
-      {user ? <SendMessageForm /> : <SigninBox />}
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <View style={styles.container}>
+        <Header />
+        <MessageList />
+        {user ? <SendMessageForm /> : <SigninBox />}
+      </View>
+    </KeyboardAvoidingView>
   );
 }
